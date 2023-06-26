08dcafa52416dd13403abf265d500936c77577bd
max <maxpasqualis@gmail.com>
2023-06-25 19:17:17 -0700

    Create card object that takes enums as args

---
+++ src/behavior/card.js
@@ -0,0 +1,9 @@
+class CardObj {
+  constructor(/*color enum*/ color, /*suit enum*/ suit, /*type enum*/ type) {
+    this.color = color;
+    this.suit = suit;
+    this.type = type;
+  }
+}
+
+export default CardObj;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import CardObj from "../behavior/card";
+import Color from "../behavior/enum/color";
+import Suit from "../behavior/enum/suit";
+import Type from "../behavior/enum/type";
 
 class Card extends React.Component {
-    render() { return <p>this is a Card component</p> }
+    render() { 
+        const card = new CardObj(Color.Red, Suit.Hearts, Type.Ace)
+        return <>
+            <p>this is a Card component.</p>
+            <p>properties:</p>
+            <p>{card.color.name}</p>
+            <p>{card.suit.name}</p>
+            <p>{card.type.name}</p>
+        </> 
+    }
 }
 
 export default Card;

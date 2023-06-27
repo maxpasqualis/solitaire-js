c1aeeed292a2ffb6112bff7c762ac22d9e5622f1
max <maxpasqualis@gmail.com>
2023-06-27 00:32:27 -0700

    Add demo styling to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,14 @@ import Color from "../behavior/enum/color";
 import Suit from "../behavior/enum/suit";
 import Type from "../behavior/enum/type";
 
+import "./styles/Card.css"
+
 class Card extends React.Component {
     render() { 
         const card = new CardObj(Color.Red, Suit.Hearts, Type.Ace)
-        return <>
-            <p>this is a Card component.</p>
-            <p>properties:</p>
-            <p>{card.color.name}</p>
-            <p>{card.suit.name}</p>
-            <p>{card.type.name}</p>
-        </> 
+        return <div className="card">
+            <p>{card.toString()}</p>
+        </div> 
     }
 }
 

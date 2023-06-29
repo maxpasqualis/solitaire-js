d80080942eec9daaf56796951e4eea5d53ca6c97
max <maxpasqualis@gmail.com>
2023-06-29 00:53:35 -0700

    Create Stock and Foundation components

---
+++ src/components/Foundation.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Foundation extends React.Component {
+    render() {
+        return;
+    }
+}
+
+export default Foundation;
---
+++ src/components/Stock.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Stock extends React.Component {
+    render() {
+        return;
+    }
+}
+
+Stock.propTypes = {
+    cards: PropTypes.array.isRequired,
+}
+
+export default Stock;
